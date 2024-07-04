38792498d0f26973633c7eb6e09efd60272b51f9
andersonfs94 <andersonfs94@gmail.com>
2024-07-04 19:19:41 -0300

    Barra lateral

--- Projeto Website para nutricionista e personal trainer/assets/js/script.js
+++ Projeto Website para nutricionista e personal trainer/assets/js/script.js
@@ -4,6 +4,21 @@ document.addEventListener("DOMContentLoaded", function() {
     setTimeout(function() {
         var loadingIndicator = document.getElementById('loadingIndicator');
         loadingIndicator.style.display = 'none';
-    }, 3000); // Adiciona um atraso de 3 segundos
+    }, 2000); // Adiciona um atraso de 3 segundos
 });
 
+document.getElementById("carrinho").onclick = function() {
+    var sidebar = document.getElementById("sidebar");
+    var carrinho = document.getElementById("carrinho");
+    var content = document.getElementById("content");
+    
+    if (sidebar.style.width === "250px") {
+        sidebar.style.width = "0";
+        content.style.marginLeft = "0";
+        carrinho.innerHTML = '<img src="../assets/icons/fas fa-shopping-cart.PNG" alt="Adicionar ao Carrinho" style="width: 24px; height: 24px;">';
+    } else {
+        sidebar.style.width = "250px";
+        content.style.marginLeft = "250px";
+        carrinho.innerHTML = '<img src="../assets/icons/fas fa-shopping-cart.PNG" alt="Adicionar ao Carrinho" style="width: 24px; height: 24px;">';
+    }
+}
